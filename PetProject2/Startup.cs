9250f0f793672010d69baac8f8410cc49354ac4b
Tkachuk-Vladimir <tck.vova@ya.ru>
2022-08-18 15:08:06 +0300

    appsettings add

---
+++ PetProject2/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using PetProject2.Service;
+
+namespace PetProject2
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //подключение конфиг из appsettings.json
+            Configuration.Bind("Project", new Config());
+
+            //добавление поддержки констроллеров и представлений
+            services.AddControllersWithViews().
+                SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //в процессе разработки что бы видеть информацию об ошибке
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            //подключаем поддержку статических файлов в придложении(css,js и тд)
+            app.UseStaticFiles();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("default ", "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
