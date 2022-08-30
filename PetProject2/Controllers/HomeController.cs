59dc1ba9bfcf3d63ce2806d1a474dd9cd3ab6fc2
Tkachuk-Vladimir <tck.vova@ya.ru>
2022-08-30 12:40:20 +0300

    add change _layout

--- PetProject2/Controllers/HomeController.cs
+++ PetProject2/Controllers/HomeController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PetProject2.Domain.Repositories;
 
 namespace PetProject2.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: /<controller>/
+        private readonly DataManager dataManager;
+
+        public HomeController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(dataManager.TextFields.GetTextFieldByCodeWord("PageIndex"));
+        }
+
+        public IActionResult Contacts()
+        {
+            return View(dataManager.TextFields.GetTextFieldByCodeWord("PageContacts"));
         }
     }
 }
---
+++ PetProject2/Models/ViewComponents/SidebarViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PetProject2.Domain.Repositories;
+
+namespace PetProject2.Models.ViewComponents
+{
+    public class SidebarViewComponent : ViewComponent
+    {
+        private readonly DataManager dataManager;
+
+        public SidebarViewComponent(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
+        public Task<IViewComponentResult> InvokeAsync()
+        {
+            return Task.FromResult((IViewComponentResult)View("Default", dataManager.ServiceItems.GetServiceItems()));
+        }
+    }
+}
